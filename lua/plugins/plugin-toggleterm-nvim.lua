b188f0a55c9bef570fa9aa13ee8319e1ce92bb0c
w4ngzhen <w4ngzhen@hotmail.com>
2023-06-26 20:24:05 +0800

    plugin: add toggleterm plugin, support use lazygit.

---
+++ lua/plugins/plugin-toggleterm-nvim.lua
@@ -0,0 +1,43 @@
+return {
+  -- amongst your other plugins
+  {
+    'akinsho/toggleterm.nvim', 
+    version = "*", 
+    config = function ()
+      -- config toggleterm.
+      require("toggleterm").setup({
+        size = function(term)
+          if term.direction == "horizontal" then
+            return 15
+          elseif term.direction == "vertical" then
+            return vim.o.columns * 0.4
+          end
+        end,
+        direction = 'float',
+        open_mapping = [[<C-\>]],
+      })
+
+      -- 通过自定义terminal，调用外部安装的lazygit
+      local Terminal  = require('toggleterm.terminal').Terminal
+      local lazygit = Terminal:new({ 
+        cmd = "lazygit", 
+        hidden = true,
+        -- 添加on_close才能在lazygit异常的时候终端不闪退
+        on_close = function() 
+          vim.cmd("echo on_close")
+        end,
+      })
+      
+      function _lazygit_toggle()
+        lazygit:toggle()
+      end
+      
+      vim.api.nvim_set_keymap(
+        "n", 
+        "<C-M-\\>", --CTRL+ALT+\
+        "<cmd>lua _lazygit_toggle()<CR>", 
+        {noremap = true, silent = true}
+      )
+    end
+  }
+}
