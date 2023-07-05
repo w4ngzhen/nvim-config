5e99381a12791a92ec900d2734aad0fe7cdef8e1
w4ngzhen <w4ngzhen@hotmail.com>
2023-07-05 10:56:21 +0800

    language server: add format keymap for every buff.

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -10,5 +10,9 @@ local config_tb = {
 --
 for _, cfg in ipairs(config_tb) do
   local setup_config = cfg.setup_config
+  -- 增加格式化按键
+  setup_config.on_attach = function (_, buf)
+    vim.api.nvim_buf_set_keymap(buf, "n", "<C-M-l>", "<cmd>lua vim.lsp.buf.format()<CR>", { noremap = true })
+  end
   require("lspconfig")[cfg.name].setup(setup_config)
 end
