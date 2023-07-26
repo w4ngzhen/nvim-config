e726dd4d338503f9bfbcf44018c4ef75c1716704
w4ngzhen <w4ngzhen@hotmail.com>
2023-07-26 14:15:06 +0800

    plugin: add plugin for search highlight display.

---
+++ lua/plugins/plugin-nvim-hlslens.lua
@@ -0,0 +1,19 @@
+return {
+  'kevinhwang91/nvim-hlslens',
+  config = function()
+    require('hlslens').setup()
+    local keymap_opts = { noremap = true, silent = true }
+    vim.api.nvim_set_keymap('n', 'n',
+      [[<Cmd>execute('normal! ' . v:count1 . 'n')<CR><Cmd>lua require('hlslens').start()<CR>]],
+      keymap_opts)
+    vim.api.nvim_set_keymap('n', 'N',
+      [[<Cmd>execute('normal! ' . v:count1 . 'N')<CR><Cmd>lua require('hlslens').start()<CR>]],
+      keymap_opts)
+    vim.api.nvim_set_keymap('n', '*', [[*<Cmd>lua require('hlslens').start()<CR>]], keymap_opts)
+    vim.api.nvim_set_keymap('n', '#', [[#<Cmd>lua require('hlslens').start()<CR>]], keymap_opts)
+    vim.api.nvim_set_keymap('n', 'g*', [[g*<Cmd>lua require('hlslens').start()<CR>]], keymap_opts)
+    vim.api.nvim_set_keymap('n', 'g#', [[g#<Cmd>lua require('hlslens').start()<CR>]], keymap_opts)
+
+    vim.api.nvim_set_keymap('n', '<Leader>l', '<Cmd>noh<CR>', keymap_opts)
+  end
+}
