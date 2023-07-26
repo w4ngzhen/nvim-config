eb1f968b3d641ce0ab7381904af6efc40914addc
w4ngzhen <w4ngzhen@hotmail.com>
2023-07-26 16:36:42 +0800

    plugin: add plugin for workspace and modify nvim-tree and telescope for adapting workspaces.nvim.

--- lua/plugins/plugin-nvim-tree.lua
+++ lua/plugins/plugin-nvim-tree.lua
@@ -4,7 +4,10 @@ return {
         version = "*",
         dependencies = {"nvim-tree/nvim-web-devicons"},
         config = function()
-            require("nvim-tree").setup {}
+            require("nvim-tree").setup {
+              -- 针对workspaces.nvim切换工作空间以后，NvimTree未更新的情况
+              update_cwd = true
+            }
         end
     }
 }
--- lua/plugins/plugin-telescope-nvim.lua
+++ lua/plugins/plugin-telescope-nvim.lua
@@ -1,10 +1,12 @@
 -- 插件相关快捷键配置
 local function set_keymap()
-    local builtin = require('telescope.builtin')
-    local opt = { noremap = true }
-    vim.keymap.set('n', '<F1>', builtin.find_files, opt)
-    vim.keymap.set('n', '<F2>', builtin.grep_string, opt)
-    vim.keymap.set('n', '<F3>', builtin.live_grep, opt) -- 依赖ripgrep
+  local builtin = require('telescope.builtin')
+  local opt = { noremap = true }
+  vim.keymap.set('n', '<leader><F1>', builtin.find_files, opt)
+  vim.keymap.set('n', '<leader><F2>', builtin.grep_string, opt)
+  vim.keymap.set('n', '<leader><F3>', builtin.live_grep, opt) -- 依赖ripgrep
+  -- 打开集成的workspaces.nvim
+  vim.keymap.set('n', '<leader><F4>', '<cmd>Telescope workspaces<CR>', opt)
 end
 -- 此外，建议配合安装以下两个命令行工具：
 -- ripgrep: https://github.com/BurntSushi/ripgrep
@@ -15,7 +17,9 @@ return {
   dependencies = {
     'nvim-lua/plenary.nvim',
   },
-  config = function ()
+  config = function()
     set_keymap()
+    -- workspaces.nvim 集成到telescope中
+    require 'telescope'.load_extension('workspaces')
   end
 }
