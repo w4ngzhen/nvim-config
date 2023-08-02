7804d04f789da443cd201fb915911a6bee06c64e
w4ngzhen <w4ngzhen@hotmail.com>
2023-08-02 13:55:53 +0800

    plugin config: update formatter.nvim config.

--- lua/plugins/plugin-formatter-nvim.lua
+++ lua/plugins/plugin-formatter-nvim.lua
@@ -1,21 +1,35 @@
 return {
   "mhartington/formatter.nvim",
   config = function()
+    local filetype_config = {}
+    -- 前端format
     local ft_names = {
+      "html",
+      "css",
+      "json",
       "javascript",
       "javascriptreact",
       "typescript",
       "typescriptreact"
     }
-    local filetype = {}
     for _, ft_name in ipairs(ft_names) do
-      filetype[ft_name] = {
+      filetype_config[ft_name] = {
         -- 上述的filetype，都直接使用对应目录下的preitter
         require("formatter.filetypes." .. ft_name).prettier
       }
     end
+    -- c/cpp format
+    local clang_format = {
+      require("formatter.filetypes.cpp").clangformat
+    }
+    filetype_config.c = {
+      clang_format,
+    }
+    filetype_config.cpp = {
+      clang_format,
+    }
     require('formatter').setup({
-      filetype = filetype
+      filetype = filetype_config
     })
     -- keymap设置
     local keymap = vim.keymap.set
